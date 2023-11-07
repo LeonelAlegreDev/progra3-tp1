85e7724c971ddb3392c75a763fa1bd0098e4299a
LeonelAlegreDev <leonelalegre.dev@gmail.com>
2023-11-07 18:50:53 -0300

    Primer Sprint

---
+++ app/controllers/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+// namespace App\Controllers;
+// use Slim\App;
+
+
+// use Psr\Http\Message\ResponseInterface as Response;
+// use Psr\Http\Message\ServerRequestInterface as Request;
+
+echo "controller";
+
+// class DefaultController extends App
+// {
+//     public function index(Request $request, Response $response)
+//     {
+//         $payload = json_encode(array('method' => 'GET', 'msg' => "Bienvenido a la ruta default"));
+//         $response->getBody()->write($payload);
+//         return $response->withHeader('Content-Type', 'application/json');
+//     }
+// }
+
+?>
